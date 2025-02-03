18ff2791f2cd47d7f9b121918bf43ae46e88362a
Gilberto Aparecido Bernardo Junior <gbernardoti@gmail.com>
2025-02-03 18:55:09 -0300

    Projeto concluído

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Por favor, digite o número da conta: ");
+        int numero=Integer.parseInt(sc.nextLine());
+        System.out.print("Por favor, digite o número da agência: " );
+        String agencia=sc.nextLine(); 
+        System.out.print("Por favor, digite o nome do cliente: " );
+        String nomeCliente=sc.nextLine();
+        System.out.print(" Por favor, digite o saldo para saque: " );
+        float saldo=Float.parseFloat(sc.nextLine());
+
+        System.out.print("Olá " + nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        
+
+
     }
 }
